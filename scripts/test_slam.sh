d1c12a5fa966fd17ff4e01ff12ebff6fe09a0b20
wukongxzero <pavankushal15729@gmail.com>
2023-01-20 09:17:39 -0800

    test_slam change

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
+xterm  -e  " source devel/setup.bash; 
+             roslaunch turtlebot_gazebo turtlebot_world.launch " &
 sleep 3
-xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch " &
+xterm  -e  " source devel/setup.bash;
+             roslaunch turtlebot_gazebo gmapping_demo.launch " &
 sleep 3
-xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+xterm  -e  " source devel/setup.bash;
+             roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 3
-xterm  -e  " roslaunch turtlebot_teleop keyboard_teleop.launch" &
+xterm  -e  " source devel/setup.bash;
+             roslaunch turtlebot_teleop keyboard_teleop.launch" &
 sleep 3
