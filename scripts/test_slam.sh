3013c700fde82b4cc766e0f4d7fa6465316c6c1b
wukongxzero <pavankushal15729@gmail.com>
2023-01-21 03:07:48 -0800

    test_slam

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -4,21 +4,13 @@
 xterm -e "cd $(pwd)/../..;
 source devel/setup.bash;
 export ROBOT_INITIAL_POSE='-x -5 -y -2 -z 0 -R 0 -P 0 -Y 0';
-roslaunch turtlebot_gazebo turtlebot_world.launch  world_file:=$(pwd)/../../src/map/home-service.world " & 
+roslaunch turtlebot_gazebo turtlebot_world.launch  world_file:=$(pwd)/../../src/map/home-service.world" & 
 
 sleep 15
 
 # launch gmapping_demo.launch to perform slam_gmapping
 xterm -e "cd $(pwd)/../..;
 source devel/setup.bash;
-rosparam set /slam_gmapping/iterations 10;
-rosparam set /slam_gmapping/linearUpdate 0.05;
-rosparam set /slam_gmapping/angularUpdate 0.1;
-rosparam set /slam_gmapping/map_update_interval 0.25;
-rosparam set /slam_gmapping/srr 0.01;
-rosparam set /slam_gmapping/srt 0.01;
-rosparam set /slam_gmapping/str 0.02;
-rosparam set /slam_gmapping/stt 0.02;
 roslaunch turtlebot_gazebo gmapping_demo.launch" &
 
 sleep 5
